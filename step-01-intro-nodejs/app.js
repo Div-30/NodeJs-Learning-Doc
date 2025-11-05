2c055598bf9fc13df2215ab23c35648bb5f88ccd
Div-30 <divinpaul88@gmail.com>
2025-11-05 14:14:42 +0200

    feat: finish first exercise

---
+++ step-01-intro-nodejs/app.js
@@ -0,0 +1,3 @@
+const greet = require("../step-01-intro-nodejs/greetings.js");
+
+console.log(`The teacher said:${greet("Mike")}`);
---
+++ step-01-intro-nodejs/greetings.js
@@ -0,0 +1,5 @@
+const greetings = (name) => {
+  return `Hello ${name}!`;
+};
+
+module.exports = greetings;
