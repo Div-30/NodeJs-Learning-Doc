b58055262cfce481e820f489e84edc41dd90e282
Div-30 <divinpaul88@gmail.com>
2025-11-06 11:11:39 +0200

    feat: add file manager

---
+++ step-01-intro-nodejs/Exercise_3/Exercise-4/fileManager.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const fs = require("fs");
+
+const readFile = (fileName, callback) => {
+  const filePath = path.join(__dirname, fileName);
+  fs.readFile(filePath, "utf-8", (error, content) => {
+    if (error) {
+      return callback(error, null);
+    } else {
+      callback(null, content);
+    }
+  });
+};
+
+const writeFile = (fileName, data, callback) => {
+  const filePath = path.join(__dirname, fileName);
+  fs.writeFile(filePath, data, (error) => {
+    if (error) {
+      return callback(error);
+    }
+    callback(null);
+  });
+};
+
+const fileExists = (fileName) => {
+  const filePath = path.join(__dirname, fileName);
+  return fs.existsSync(filePath);
+};
+
+module.exports = {
+  readFile,
+  writeFile,
+  fileExists,
+};
