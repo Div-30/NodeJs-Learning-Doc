ad1d81327286f10cf8d107831dd325c9550b5218
Div-30 <divinpaul88@gmail.com>
2025-11-08 13:47:33 +0200

    feat: finish the exercise 2

---
+++ step-02-http-server/Exercise-2/routingServer.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  let path = "./step-02-http-server/views";
+  let statusCode = 200;
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    case "/contact":
+      path += "contact.html";
+    default:
+      path += "errorPage.html";
+      statusCode = 404;
+      break;
+  }
+  res.writeHead(statusCode, { "content-type": "text/html" });
+  const readStream = fs.createReadStream(path, { encoding: "utf-8" });
+  readStream.pipe(res);
+  readStream.on("error", (error) => {
+    console.error("File read error: ", error);
+    if (!res.headersSent) {
+      res.writeHead(500, { "content-type": "text/plain" });
+      res.end("Internal Server Error");
+    } else {
+      res.end();
+    }
+  });
+});
+server.listen(3000, "localhost", () => {
+  console.log("Server running at http://localhost:3000/");
+});
